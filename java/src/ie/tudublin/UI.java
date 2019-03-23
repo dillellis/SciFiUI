591d42f2d99f0c1336398585a5734e50f1d3e960
Dillon Ellis <C17322316@mydit.ie>
2019-03-23 14:04:30 +0000

    Stars

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -2,54 +2,59 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class UI extends PApplet
-{
+
+
+
+public class UI extends PApplet {
     Button b;
     MovingCircle mc;
 
+
     boolean[] keys = new boolean[1024];
 
-    public void keyPressed()
-    {
-        keys[keyCode] = true;
-    }
-    
-    public void keyReleased()
-    {
+    public void keyPressed() {
         keys[keyCode] = true;
     }
 
-    public boolean checkKey(int c)
-    {
-        return keys[c] || keys [Character.toUpperCase(c)];
+    public void keyReleased() {
+        keys[keyCode] = true;
     }
-    
 
-    public void settings()
-    {
-        size(800, 800);
+    public boolean checkKey(int c) {
+        return keys[c] || keys[Character.toUpperCase(c)];
+    }
+
+    public void settings() {
         // Use fullscreen instead of size to make your interface fullscreen
-        //fullScreen(); 
+        size(500,500);
     }
 
-    public void setup()
-    {
+    public void setup() {
+        background(0);
+        noStroke();
         b = new Button(this, 50, 50, 100, 50, "I am a button");
         mc = new MovingCircle(this, width / 2, height / 2, 50);
+
     }
 
-    public void draw()
-    {
-        background(0);
-        b.render();
+    public void draw() {
+        //background(0);
+        // Fading Stars in the Background
+        fill(0,10);
+        rect(0, 0, width, height);
+ 
+        fill(255);
+        ellipse(random(width), random(height), 3 ,3);
 
-        mc.update();
-        mc.render();
 
-        if (checkKey(LEFT))
-        {
-            System.out.println("Left arrow key pressed");
-        }
+
+        
+        
+
+
+        
+
+       
     }
 }
 
