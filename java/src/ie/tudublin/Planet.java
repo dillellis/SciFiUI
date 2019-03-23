2f6207c2fffc02ce9ae574799723c91959176668
Dillon Ellis <C17322316@mydit.ie>
2019-03-23 15:24:09 +0000

    PAppletRemoved

--- java/src/ie/tudublin/Planet.java
+++ java/src/ie/tudublin/Planet.java
@@ -1,16 +1,11 @@
 package ie.tudublin;
 
-import java.awt.Color;
-import java.util.Map;
-
-import processing.core.PApplet;
 
 public class Planet
 {
     private float x;
     private float y;
     private float diameter;
-    private float radius;
     UI ui;
 
     public Planet(UI ui, float x, float y, float diameter)
