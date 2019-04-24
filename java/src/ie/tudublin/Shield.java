b48021846124d806925f7a2d048182dd82cd35dc
Dillon Ellis <C17322316@mydit.ie>
2019-04-24 18:50:57 +0100

    writing

--- java/src/ie/tudublin/Shield.java
+++ java/src/ie/tudublin/Shield.java
@@ -1,44 +1,51 @@
 package ie.tudublin;
 
+import processing.core.PFont;
 import processing.core.PVector;
 
-public class Shield
-{
-    
+public class Shield {
+
     private PVector pos;
     UI ui;
     double angle = 0;
     double degree = 0.0174533;
 
-    public Shield(UI ui, float x, float y)
-    {
+    public Shield(UI ui, float x, float y) {
         this.ui = ui;
         pos = new PVector(x, y);
-        
+
     }
-    public void render()
-    {
-        ui.fill(0,0,0);
+
+    public void render() {
+        ui.fill(0, 0, 0);
         ui.ellipse(pos.x, pos.y, 200, 200);
 
         ui.stroke(57, 255, 20);
-        ui.arc(pos.x, pos.y, 50, 50, (float)degree*180,(float)degree*360);
-        ui.line(pos.x + 25, pos.y, pos.x + 50, pos.y+10);
-        ui.line(pos.x - 25, pos.y, pos.x - 50, pos.y+10);
+        ui.arc(pos.x, pos.y, 50, 50, (float) degree * 180, (float) degree * 360);
+        ui.line(pos.x + 25, pos.y, pos.x + 50, pos.y + 10);
+        ui.line(pos.x - 25, pos.y, pos.x - 50, pos.y + 10);
         ui.line(pos.x + 50, pos.y + 10, pos.x + 25, pos.y + 20);
-        ui.line(pos.x - 50, pos.y + 10, pos.x - 25, pos.y + 20 );
+        ui.line(pos.x - 50, pos.y + 10, pos.x - 25, pos.y + 20);
         ui.line(pos.x + 25, pos.y + 20, pos.x - 25, pos.y + 20);
-        ui.stroke(255,255,255);
+        ui.stroke(255, 255, 255);
 
         ui.pushMatrix();
         ui.translate(pos.x, pos.y);
-        ui.rotate((float)angle);
+        ui.rotate((float) angle);
         ui.noStroke();
         ui.fill(57, 255, 20);
         ui.ellipse(100, 100, 75, 75);
         ui.ellipse(150, 0, 50, 50);
-        ui.ellipse(125, -75, 25, 25);        
+        ui.ellipse(125, -75, 25, 25);
         ui.popMatrix();
+
+        ui.fill(0,0,0);
+        ui.rect(pos.x - 110 , pos.y + 183, 200, 25);
+
+        ui.fill(57, 255, 20);
+        ui.textSize(20);
+        ui.text("Shield regeneration",pos.x - 100 , pos.y + 200);
+        
         
 
     }
